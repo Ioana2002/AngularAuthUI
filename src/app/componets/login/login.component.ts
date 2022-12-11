fd4d877e7bd7b2ca708d803dc6f93c3656620275
Ioana Antohi <maria.antohi@stud.ubbcluj.ro>
2022-12-11 16:46:48 +0200

    hop 2

--- src/app/componets/login/login.component.ts
+++ src/app/componets/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateform';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +12,14 @@ export class LoginComponent implements OnInit{
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa fa-eye-slash";
-  constructor() { }
+  loginForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
     
   }
 
@@ -21,5 +28,24 @@ export class LoginComponent implements OnInit{
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
   }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+
+      console.log(this.loginForm.value)
+      //Send the object to database
+
+
+    }else{
+
+     
+      //throw the error using toaster and with required field
+      ValidateForm.validateAllFormFields(this.loginForm);
+      alert("Your form is invalid")
+
+    }
+  }
+
+
 
 }
--- src/app/componets/signup/signup.component.ts
+++ src/app/componets/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateform';
 
 @Component({
   selector: 'app-signup',
@@ -10,9 +12,17 @@ export class SignupComponent implements OnInit{
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa fa-eye-slash";
-  constructor() { }
+  signUpForm!: FormGroup;
+  constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      userName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
     
   }
 
@@ -21,6 +31,16 @@ export class SignupComponent implements OnInit{
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
   }
+
+  onSignup(){
+    if(this.signUpForm.valid){
+      console.log(this.signUpForm.value)
+    }else{
+      ValidateForm.validateAllFormFields(this.signUpForm)
+
+    }
+  }
+  
 
 
 }
