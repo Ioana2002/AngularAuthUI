d3685e9e660de01b8ca909405d16523c1df044ec
Ioana Antohi <maria.antohi@stud.ubbcluj.ro>
2022-12-20 19:04:46 +0200

    confirm password

--- src/app/componets/signup/signup.component.ts
+++ src/app/componets/signup/signup.component.ts
@@ -22,6 +22,7 @@ export class SignupComponent implements OnInit{
       userName: ['', Validators.required],
       email: ['', Validators.required],
       password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
     })
     
   }
